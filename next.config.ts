c9234eb45690ce557dc7b5426c3cd95eef0ae837
Giovanni Polimeni <giovannipolimeni932@gmail.com>
2026-02-03 13:26:09 +0100

    fix: remove assetPrefix and add .nojekyll to prevent 404s

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
   basePath: '/AlSolved_Ristrutturissima_Demo',
-  assetPrefix: '/AlSolved_Ristrutturissima_Demo',
 };
 
 export default nextConfig;
